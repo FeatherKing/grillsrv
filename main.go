12997a80c17c8a67f2453526af9f5df938095fdc
Chris Featherstone <featherstone.chris@gmail.com>
2016-08-13 11:01:08 -0600

    changed some stuff to use buffers. found that gmg reads a 1024 size byte array. started doing this and trimming off the excess. this avoids hitting the connection timeout usually because we only read 1MB

--- main.go
+++ main.go
@@ -2,59 +2,68 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net"
 	"time"
 )
 
 func main() {
-	ssid := "SSID"
-	password := "WIFI_PASS"
+	var buf bytes.Buffer
+	//ssid := "SSID"
+	//password := "WIFI_PASS"
 	//serverip := "52.26.201.234"
 	//port := "8060"
-	ssidlen := len(ssid)
-	passwordlen := len(password)
+	//ssidlen := len(ssid)
+	//passwordlen := len(password)
 	//serveriplen := len(serverip)
 	//portlen := len(port)
-	s := fmt.Sprintf("UH%c%c%s%c%s!", 0, ssidlen, ssid, passwordlen, password) // connect to wifi
+	fmt.Fprint(&buf, "UL!") // get grill id
+	//s := fmt.Sprintf("UH%c%c%s%c%s!", 0, ssidlen, ssid, passwordlen, password) // connect to wifi
 	//s := fmt.Sprintf("UG%c%s%c%s%c%s%c%s!", ssidlen, ssid, passwordlen, password, serveriplen, serverip, portlen, port)
-	bytes := []byte(s)
-	sendData(bytes)
+	//bytes := []byte(s)
+	sendData(&buf)
+	//sendData(bytes)
 }
 
-func sendData(b []byte) {
+func sendData(b *bytes.Buffer) {
 	//b = []byte("UWFM!") // leaveServerMode
-	//grillIdCommand := []byte("UL!")
 	//str := "UL!" // get grill id
 	//str := "GET / HTTP/1.0\r\n\r\n"
 	conn, err := net.Dial("tcp", "LAN_IP:PORT")
-	//conn, err := net.Dial("tcp", "google.com:80")
+	timeout := time.Now().Add(3 * time.Second)
+	conn.SetReadDeadline(timeout)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	fmt.Println("Connected")
-	timeout := time.Now().Add(time.Second)
-	conn.SetReadDeadline(timeout)
 
 	defer conn.Close()
-	fmt.Println("Sending Data")
-	//ret, _ := conn.Write([]byte(str))
-	ret, _ := conn.Write(b)
-
-	//ret, err := fmt.Fprint(conn, buf)
+	fmt.Println("Sending Data..")
+	ret, err := conn.Write(b.Bytes())
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	fmt.Printf("Bytes Written: %v\n", ret)
-	fmt.Println("Reading Data")
-	//read := make([]byte, 2048)
-	//status, _ := conn.Read(read)
-	//status, err := bufio.NewReader(conn).ReadBytes(33)
-	status, err := bufio.NewReader(conn).ReadString(33)
+	b.Reset()
 
-	fmt.Println(status)
-	fmt.Println("total size:", len(status))
+	fmt.Println("Reading Data..")
+	barray := make([]byte, 1024)
+	status, err := bufio.NewReader(conn).Read(barray)
+	//bufio.NewReader(conn).ReadBytes(33)
+	if err != nil {
+		// prints a timeout error but data was still read
+		fmt.Println(err.Error())
+	}
+	// trim null of 1024 byte array
+	barray = bytes.Trim(barray, "\x00")
+
+	// print what we got back
+	fmt.Println(string(b.Bytes()))
+	fmt.Println(string(barray))
+	fmt.Println(barray)
+	fmt.Println("Bytes Read:", status)
+	fmt.Println("Read Buffer Size:", len(barray))
 
 	//fmt.Println(grillIdCommand)
 	/*this.prefs.edit().putString("serverModeKey", new StringBuilder(String.valueOf(DataManager.getInstance().mGrillId.substring(NUM_PAGES, 11))).append(DataManager.getInstance().mExternalIp).toString()).apply();
@@ -94,34 +103,5 @@ func sendData(b []byte) {
 		         	"encoding/binary"
 		         	"bytes"
 		         )
-
-		         type myStruct struct {
-		         	ssidlen int
-		         	ssid string
-		         	passwordlen int
-		         	password string
-		         	serveriplen int
-		         	serverip string
-		         	portlen int
-		         	port string
-		         	//ssidlen, ssid, passwordlen, password, serveriplen, serverip, portlen, port)
-		         }
-
-		         func main() {
-		         	ssid := "SSID"
-		         	password := "WIFI_PASS"
-		         	serverip := "52.26.201.234"
-		         	port := "8060"
-		         	var bin_buf bytes.Buffer
-
-		         	x := myStruct{len(ssid),"SSID", len(password), "WIFI_PASS", len(serverip), "52.26.201.234", len(port), "8060"}
-		         	binary.Write(&bin_buf, binary.BigEndian, x)
-		         	//fmt.Printf("% x", sha1.Sum(bin_buf.Bytes()))
-		         	fmt.Printf("UG%!", bin_buf.Bytes())
-		         	fmt.Printf(x)
-		         }
-
-
 	*/
-
 }
