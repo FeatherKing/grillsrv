c4cf7c72f5edfe67b89bb97e8fc4f94acba1467f
Chris Featherstone <featherstone.chris@gmail.com>
2016-08-13 23:58:36 -0600

    created struct to house grill data. pull external ip from website and decode json into struct

--- main.go
+++ main.go
@@ -3,56 +3,90 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 	"time"
 )
 
 const (
-	grillID = iota
-	useWifi = iota
+	grillID    = iota
+	useWifi    = iota
+	serverKey  = iota
+	serverMode = iota
 )
+
+// Grill ...
+// struct
+type Grill struct {
+	IP          string
+	serial      string
+	ssid        string
+	password    string
+	ssidlen     int
+	passwordlen int
+	serverip    string
+	port        string
+	serveriplen int
+	portlen     int
+}
 
 func main() {
 	var message = flag.Int("messageType", -1,
 		"0 to print grill id. 1 to switch from ptp to wifi")
 	flag.Parse()
+	myGrill := Grill{
+		serial:   "GMGSERIAL",
+		ssid:     "SSID",
+		password: "WIFI_PASS",
+		serverip: "52.26.201.234",
+		port:     "8060",
+	}
+	myGrill.ssidlen = len(myGrill.ssid)
+	myGrill.passwordlen = len(myGrill.password)
+	myGrill.serveriplen = len(myGrill.serverip)
+	myGrill.portlen = len(myGrill.port)
 	var buf bytes.Buffer
-	ssid := "SSID"
-	password := "WIFI_PASS"
-	//serverip := "52.26.201.234"
-	//port := "8060"
-	ssidlen := len(ssid)
-	passwordlen := len(password)
-	//serveriplen := len(serverip)
-	//portlen := len(port)
 
-	//s := fmt.Sprintf("UH%c%c%s%c%s!", 0, ssidlen, ssid, passwordlen, password) // connect to wifi
-	//s := fmt.Sprintf("UG%c%s%c%s%c%s%c%s!", ssidlen, ssid, passwordlen, password, serveriplen, serverip, portlen, port)
-	//bytes := []byte(s)
-	fmt.Println(*message, grillID, useWifi)
 	switch *message {
 	case grillID:
 		// get grill id
 		fmt.Println("Message: Get Grill Id")
 		fmt.Fprint(&buf, "UL!")
 	case useWifi:
-		fmt.Fprintf(&buf, "UH%c%c%s%c%s!", 0, ssidlen, ssid, passwordlen, password)
+		fmt.Println("Message: PTP to Wifi")
+		fmt.Fprintf(&buf, "UH%c%c%s%c%s!", 0, myGrill.ssidlen, myGrill.ssid, myGrill.passwordlen, myGrill.password)
+	case serverMode:
+		fmt.Println("Message: Wifi to Server Mode")
+		fmt.Fprintf(&buf, "UG%c%s%c%s%c%s%c%s!", myGrill.ssidlen, myGrill.ssid, myGrill.passwordlen, myGrill.password, myGrill.serveriplen, myGrill.serverip, myGrill.portlen, myGrill.port)
+	case serverKey:
+		fmt.Println("Message: Create Server Key")
+		// curl 'https://api.ipify.org?format=json'
+		r, err := http.Get("https://api.ipify.org?format=json")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		defer r.Body.Close()
+		err = json.NewDecoder(r.Body).Decode(&myGrill)
+		serverKey := []byte(fmt.Sprint(myGrill.serial, myGrill.IP))
+		fmt.Println("Serial:", myGrill.serial)
+		fmt.Println("IP:", myGrill.IP)
+		fmt.Println("ServerKey Bytes:", serverKey)
+		fmt.Println("ServerKey:", fmt.Sprint(myGrill.serial, myGrill.IP))
+
+		os.Exit(0) // remove this once key is build correctly
 	default:
 		fmt.Println("You must choose a message type.")
 		os.Exit(1)
 	}
-
 	sendData(&buf)
-	//sendData(bytes)
 }
 
 func sendData(b *bytes.Buffer) {
 	//b = []byte("UWFM!") // leaveServerMode
-	//str := "UL!" // get grill id
-	//str := "GET / HTTP/1.0\r\n\r\n"
 	conn, err := net.Dial("tcp", "LAN_IP:PORT")
 	timeout := time.Now().Add(3 * time.Second)
 	conn.SetReadDeadline(timeout)
@@ -73,9 +107,7 @@ func sendData(b *bytes.Buffer) {
 	fmt.Println("Reading Data..")
 	barray := make([]byte, 1024)
 	status, err := bufio.NewReader(conn).Read(barray)
-	//bufio.NewReader(conn).ReadBytes(33)
 	if err != nil {
-		// prints a timeout error but data was still read
 		fmt.Println(err.Error())
 	}
 	// trim null of 1024 byte array
