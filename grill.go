d2040c61f3ef7e7f83715b5df0a1f31eededb280
Chris Featherstone <featherstone.chris@gmail.com>
2016-08-27 11:57:58 -0600

    added 3 retries to the grill request because sometimes it doesnt respond. also added a timestamp to the requests

--- grill.go
+++ grill.go
@@ -56,7 +56,8 @@ var warnStates = map[int]string{
 //UR001!
 func getInfo() ([]byte, error) {
 	var buf bytes.Buffer
-	fmt.Println("Request: Get All Info")
+	fmt.Printf("%s    Request: Get All Info\n", time.Now().Format(time.RFC822))
+	//fmt.Println("Request: Get All Info")
 	fmt.Fprint(&buf, "UR001!")
 	grillResponse, err := sendData(&buf)
 	if err != nil {
@@ -67,7 +68,7 @@ func getInfo() ([]byte, error) {
 
 // UT###!
 func setGrillTemp(temp int) ([]byte, error) {
-	fmt.Println("Request: Set Grill Temp")
+	fmt.Printf("%s    Request: Set Grill Temp\n", time.Now().Format(time.RFC822))
 	single := temp % 10
 	tens := (temp % 100) / 10
 	hundreds := temp / 100
@@ -90,7 +91,7 @@ func setGrillTemp(temp int) ([]byte, error) {
 
 // UF###!
 func setProbeTemp(temp int) ([]byte, error) {
-	fmt.Println("Request: Set Probe Temp")
+	fmt.Printf("%s    Request: Set Probe Temp\n", time.Now().Format(time.RFC822))
 	single := temp % 10
 	tens := (temp % 100) / 10
 	hundreds := temp / 100
@@ -114,7 +115,7 @@ func setProbeTemp(temp int) ([]byte, error) {
 // UK001!
 func powerOn() ([]byte, error) {
 	var buf bytes.Buffer
-	fmt.Println("Request: Turn Grill On")
+	fmt.Printf("%s    Request: Turn Grill On\n", time.Now().Format(time.RFC822))
 	fmt.Fprint(&buf, "UK001!")
 	grillResponse, err := sendData(&buf)
 	if err != nil {
@@ -126,7 +127,7 @@ func powerOn() ([]byte, error) {
 //UK004!
 func powerOff() ([]byte, error) {
 	var buf bytes.Buffer
-	fmt.Println("Request: Turn Grill Off")
+	fmt.Printf("%s    Request: Turn Grill Off\n", time.Now().Format(time.RFC822))
 	fmt.Fprint(&buf, "UK004!")
 	grillResponse, err := sendData(&buf)
 	if err != nil {
@@ -138,7 +139,7 @@ func powerOff() ([]byte, error) {
 //UL!
 func grillID() ([]byte, error) {
 	var buf bytes.Buffer
-	fmt.Println("Request: Get Grill ID")
+	fmt.Printf("%s    Request: Get Grill ID\n", time.Now().Format(time.RFC822))
 	fmt.Fprint(&buf, "UL!")
 	grillResponse, err := sendData(&buf)
 	if err != nil {
@@ -150,7 +151,7 @@ func grillID() ([]byte, error) {
 //UN!
 func grillFW() ([]byte, error) {
 	var buf bytes.Buffer
-	fmt.Println("Request: Get Grill FW")
+	fmt.Printf("%s    Request: Get Grill FW\n", time.Now().Format(time.RFC822))
 	fmt.Fprint(&buf, "UN!")
 	grillResponse, err := sendData(&buf)
 	if err != nil {
@@ -160,44 +161,55 @@ func grillFW() ([]byte, error) {
 }
 
 func sendData(b *bytes.Buffer) ([]byte, error) {
-	//b = []byte("UWFM!") // leaveServerMode
-	if b.Len() == 0 {
-		return nil, errors.New("Nothing to Send to Grill")
-	}
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s", myGrill.grillIP), 3*time.Second)
-	if err != nil {
-		return nil, errors.New("Connection to Grill Failed")
-	}
-	timeout := time.Now().Add(3 * time.Second)
-	conn.SetReadDeadline(timeout)
-	//fmt.Println("Connected")
-
-	defer conn.Close()
-	//fmt.Println("Sending Data..")
-	_, err = conn.Write(b.Bytes())
-	if err != nil {
-		return nil, errors.New("Failure Sending Payload to Grill")
-	}
-	//fmt.Printf("Bytes Written: %v\n", ret)
-	b.Reset()
-
-	//fmt.Println("Reading Data..")
 	barray := make([]byte, 1024)
-	_, err = bufio.NewReader(conn).Read(barray)
-	if err != nil {
-		return nil, errors.New("Failed Reading Result From Grill")
-	}
-	// trim null of 1024 byte array
-	//barray = bytes.Trim(barray, "\x00")
-	barray = barray[:36]
+	var err error
+	var readBytes int
+	retries := 3 // the grill doesnt always respond on the first try
+	for i := 1; i <= retries; i++ {
+		var conn net.Conn
+		if i != 1 {
+			fmt.Printf("Request Attempt %v\n", i)
+		}
+		if b.Len() == 0 && i == retries {
+			return nil, errors.New("Nothing to Send to Grill")
+		}
+		conn, err = net.DialTimeout("tcp", fmt.Sprintf("%s", myGrill.grillIP), 3*time.Second)
+		if err != nil && i == retries {
+			return nil, errors.New("Connection to Grill Failed")
+		}
+		timeout := time.Now().Add(time.Second)
+		conn.SetReadDeadline(timeout) // sometimes the grill holds the conection forever
+		//fmt.Println("Connected")
 
-	// print what we got back
-	/*
-		fmt.Println(string(b.Bytes()))
-		fmt.Println(string(barray))
-		fmt.Println(barray)
-		fmt.Println("Bytes Read:", status)
-		fmt.Println("Read Buffer Size:", len(barray))
-	*/
+		defer conn.Close()
+		//fmt.Println("Sending Data..")
+		_, err = conn.Write(b.Bytes())
+		if err != nil && i == retries {
+			return nil, errors.New("Failure Sending Payload to Grill")
+		}
+		//fmt.Printf("Bytes Written: %v\n", ret)
+		//b.Reset()
+
+		//fmt.Println("Reading Data..")
+		readBytes, err = bufio.NewReader(conn).Read(barray)
+		if err != nil && i == retries {
+			return nil, errors.New("Failed Reading Result From Grill")
+		}
+		if readBytes > 0 {
+			break
+		}
+		// trim null of 1024 byte array
+		//barray = bytes.Trim(barray, "\x00")
+
+		// print what we got back
+		/*
+			fmt.Println(string(b.Bytes()))
+			fmt.Println(string(barray))
+			fmt.Println(barray)
+			fmt.Println("Bytes Read:", status)
+			fmt.Println("Read Buffer Size:", len(barray))
+		*/
+	}
+	barray = barray[:36]
 	return barray, nil
 }
